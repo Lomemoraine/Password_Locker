96b7cff42814997616b0304bdb708cf91abc6b42
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-04-24 12:58:10 +0300

    create method to create new users and save users in run.py

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3.9
 from password import Credentials, User
+def create_user(account,fname,lname,email):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(account,fname,lname,email)
+    return new_user
+def save_user(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
