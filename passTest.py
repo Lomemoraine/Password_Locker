141cbdaf5cbc935aaa85da8966671c3e056d9bd6
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-04-24 07:44:48 +0300

    add teardown method to clean up after each test has run

--- passTest.py
+++ passTest.py
@@ -8,5 +8,10 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,"Lorraine")
         self.assertEqual(self.new_user.last_name,"Chepkemoi")
         self.assertEqual(self.new_user.email,"lorraine@gmail.com")
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_List = []
 if __name__ == "__main__":
         unittest.main()
