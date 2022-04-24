d5d393f184bc40c66b7052ae9e12a9ddddbf8a94
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-04-24 13:17:59 +0300

    start the main function to execute the code

--- run.py
+++ run.py
@@ -11,3 +11,31 @@ def save_user(user):
     Function to save user
     '''
     user.save_user()
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+def find_user(username):
+    '''
+    Function that finds a user by username and returns the user
+    '''
+    return User.find_by_username(username)
+def check_existing_users(username):
+    '''
+    Function that check if a user exists with that username and return a Boolean
+    '''
+    return User.user_exists(username)
+def display_users():
+    '''
+    Function that returns all the saved users
+    '''
+    return User.display_users()
+def main():
+    print("Hello Welcome to Password Locker. What is your name?")
+    user_name = input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+    while True:
+        print("Use these short codes : cc - create a new credential, dc - display credentials, fc -find a credential, ex -exit the passWord Locker ")
+        short_code = input().lower()
